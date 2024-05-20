fb065047c19590e7a7e6d5fdce02ec2352f37931
infantez <infante.nestor41@gmail.com>
2024-05-20 16:38:10 -0600

    modified:   src/DB/query.sql

--- src/DB/query.sql
+++ src/DB/query.sql
@@ -1,7 +1,14 @@
 CREATE DATABASE StoreApp;
-
 USE StoreApp;
 
-SELECT * FROM users;
+SELECT * from roles;
+SELECT * from user_has_roles;
+SELECT * from users;
 
+TRUNCATE TABLE user_has_roles;
+TRUNCATE TABLE roles;
+TRUNCATE TABLE users;
 
+DROP TABLE user_has_roles;
+DROP TABLE roles;
+DROP TABLE users;
